cf2f87fb946e3d5b8459e1602810815662edb7e1
Your Name <YourEmail@example.com>
2025-12-08 15:17:44 +0500

    Hassan

--- get_set.dart
+++ get_set.dart
@@ -1,38 +1,17 @@
-// class Student{
-//   String? _name;
-//   int? _age;
-//   int? _rollno;
-
-//  String? get name =>this._name;
-//  int? get age => this._age;
-//  int? get rollno => this._rollno;
-
-
-//  set name(String value) => _name = value;
-//  set age(int value) => _age = value;
-//  set rollno(int value) => _rollno = value;
-
-// }
-
-
 class Student{
   String? _name;
   int? _age;
   int? _rollno;
 
-
-  String? get name => this._name;
-  int? get age => this._age;
-  int? get rollno => this._rollno;
-
-
-   set name(String value) => this._name = value;
-   set age(int value) => this._age = value;
-   set rollno(int value) => this._rollno = value;
+ String? get name =>this._name;
+ int? get age => this._age;
+ int? get rollno => this._rollno;
 
 
-
-
-
+ set name(String value) => _name = value;
+ set age(int value) => _age = value;
+ set rollno(int value) => _rollno = value;
 
 }
+
+
