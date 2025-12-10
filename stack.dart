393563452970c3f3f3344a79ee6b823cf7801c3f
Your Name <YourEmail@example.com>
2025-12-10 11:59:38 +0500

    changing

--- stack.dart
+++ stack.dart
@@ -1,19 +1,59 @@
-void main(List<String> args) {
-  Stack stack = Stack();
-  stack.push(1);
-  stack.push(2);
-  stack.push(3);
-  stack.push(4);
-  stack.push(5);
-  stack.display();
-  print(stack.pop());
-  // print(stack.peek());
-  // print(stack.isEmpty());
-  // print(stack.size());
-  stack.display();
+// void main(List<String> args) {
+//   Stack stack = Stack();
+//   stack.push(1);
+//   stack.push(2);
+//   stack.push(3);
+//   stack.push(4);
+//   stack.push(5);
+//   stack.display();
+//   print(stack.pop());
+//   // print(stack.peek());
+//   // print(stack.isEmpty());
+//   // print(stack.size());
+//   stack.display();
 
+// }
 
-}
+// class Stack {
+//   List<int> stack = [];
+
+//   void push(int value) {
+//     stack.add(value);
+//   }
+
+//   int pop() {
+//     if (stack.isEmpty){
+//       throw Exception("Stack is empty");
+//     }
+//     return stack.removeLast();
+//   }
+
+//   int peek() {
+//     if (stack.isEmpty){
+//       throw Exception("Stack is empty");
+//     }
+//     return stack.last;
+//   }
+
+//   bool isEmpty() {
+//     return stack.isEmpty;
+//   }
+
+//   int size() {
+//     return stack.length;
+//   }
+
+//   void display() {
+//     if (stack.isEmpty){
+//       print("Stack is empty");
+//     }
+//     else{
+//       for (int i = 0; i < stack.length; i++) {
+//       print(stack[i]);
+//     }
+//     }
+//   }
+// }
 
 class Stack {
   List<int> stack = [];
@@ -23,15 +63,15 @@ class Stack {
   }
 
   int pop() {
-    if (stack.isEmpty){
-      throw Exception("Stack is empty");
+    if (stack.isEmpty) {
+      throw Exception('stack is empty');
     }
     return stack.removeLast();
   }
 
   int peek() {
-    if (stack.isEmpty){
-      throw Exception("Stack is empty");
+    if (stack.isEmpty) {
+      throw Exception('stack is empity');
     }
     return stack.last;
   }
@@ -45,13 +85,12 @@ class Stack {
   }
 
   void display() {
-    if (stack.isEmpty){
-      print("Stack is empty");
-    }
-    else{
+    if (stack.isEmpty) {
+      print('stack is empty');
+    } else {
       for (int i = 0; i < stack.length; i++) {
-      print(stack[i]);
-    }
+        print(stack[i]);
+      }
     }
   }
 }
