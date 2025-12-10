55269546ddead56a05be7657507b58add7964eda
Your Name <YourEmail@example.com>
2025-12-10 14:14:10 +0500

    changing

--- queue.dart
+++ queue.dart
@@ -1,57 +1,94 @@
-void main(List<String> args) {
-  Queue queue = Queue();
+// void main(List<String> args) {
+//   Queue queue = Queue();
 
-  queue.enqueue(1);
-  queue.enqueue(2);
-  queue.enqueue(3);
-  queue.enqueue(4);
-  queue.enqueue(5);
-  queue.display();
-  queue.dequeue();
-  queue.dequeue();
-  queue.display();
-  queue.enqueue(2);
-  queue.enqueue(1);
-  queue.display();
+//   queue.enqueue(1);
+//   queue.enqueue(2);
+//   queue.enqueue(3);
+//   queue.enqueue(4);
+//   queue.enqueue(5);
+//   queue.display();
+//   queue.dequeue();
+//   queue.dequeue();
+//   queue.display();
+//   queue.enqueue(2);
+//   queue.enqueue(1);
+//   queue.display();
  
-}
+// }
 
-class Queue {
-  List<int> queue = [];
+// class Queue {
+//   List<int> queue = [];
 
-  void enqueue(int value) {
+//   void enqueue(int value) {
+//     queue.add(value);
+//   }
+
+//   int dequeue() {
+//     if (queue.isEmpty) {
+//       throw Exception("Queue is empty");
+//     }
+//     return queue.removeAt(0);
+//   }
+
+//   int peek() {
+//     if (queue.isEmpty) {
+//       throw Exception("Queue is empty");
+//     }
+//     return queue.first;
+//   }
+
+//   bool isEmpty() {
+//     return queue.isEmpty;
+//   }
+
+//   int size() {
+//     return queue.length;
+//   }
+
+//   void display() {
+//     if (queue.isEmpty) {
+//       print("Queue is empty");
+//     } else {
+//       for (int i = 0; i < queue.length; i++) {
+//         print(queue[i]);
+//       }
+//     }
+//   }
+// }
+
+
+class Queue{
+  List <int> queue = [];
+  void enqueue(int value){
     queue.add(value);
   }
-
-  int dequeue() {
-    if (queue.isEmpty) {
-      throw Exception("Queue is empty");
+  int dequeue(){
+    if(queue.isEmpty){
+      throw Exception('queue is empty');
     }
     return queue.removeAt(0);
   }
-
-  int peek() {
-    if (queue.isEmpty) {
-      throw Exception("Queue is empty");
+  int peek(){
+    if(queue.isEmpty){
+      throw Exception('queue is empty');
     }
     return queue.first;
-  }
-
-  bool isEmpty() {
+    }
+    bool isEmpty(){
     return queue.isEmpty;
   }
-
-  int size() {
+  int size(){
     return queue.length;
   }
-
-  void display() {
-    if (queue.isEmpty) {
-      print("Queue is empty");
-    } else {
-      for (int i = 0; i < queue.length; i++) {
+  void display(){
+    if(queue.isEmpty){
+      print('queue is empty');
+    }
+    else{
+      for (int i = 0; i < queue.length; i++){
         print(queue[i]);
       }
     }
   }
-}
+
+  }
